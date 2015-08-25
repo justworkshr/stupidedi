930afe242ac2af70b75ead4d3d1e7681800239fb
Matthew Tom-Wolverton <mtw@mtwstudios.com>
2015-08-24 23:01:28 -0400

    forgot the BGN segment def

---
+++ lib/stupidedi/versions/functional_groups/005010/segment_defs/BGN.rb
@@ -0,0 +1,27 @@
+module Stupidedi
+  module Versions
+    module FunctionalGroups
+      module FiftyTen
+        module SegmentDefs
+
+          s = Schema
+          e = ElementDefs
+          r = ElementReqs
+
+          BGN = s::SegmentDef.build(:BGN, "Beginning Segment",
+            "To indicate the beginning of a transaction set",
+            e::E353 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E127 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E373 .simple_use(r::Mandatory,  s::RepeatCount.bounded(1)),
+            e::E337 .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+            e::E623 .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+            e::E127 .simple_use(r::Optional,   s::RepeatCount.bounded(1)),
+
+            e::E640 .simple_use(r::Relational, s::RepeatCount.bounded(1)),
+            e::E306 .simple_use(r::Relational, s::RepeatCount.bounded(1)))
+
+        end
+      end
+    end
+  end
+end
