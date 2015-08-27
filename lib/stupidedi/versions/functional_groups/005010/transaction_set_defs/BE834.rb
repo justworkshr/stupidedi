c35d18fc1109674858dcb417105c22ed355d2167
Matthew Tom-Wolverton <mtw@mtwstudios.com>
2015-08-27 14:47:37 -0400

    fix 2000 loop nesting

--- lib/stupidedi/versions/functional_groups/005010/transaction_set_defs/BE834.rb
+++ lib/stupidedi/versions/functional_groups/005010/transaction_set_defs/BE834.rb
@@ -29,13 +29,13 @@ module Stupidedi
                 s::INS.use(100, r::Mandatory, d::RepeatCount.bounded(1)),
                 s::REF.use(200, r::Mandatory, d::RepeatCount.bounded(1)),
                 s::REF.use(200, r::Optional , d::RepeatCount.bounded(1)),
-                s::DTP.use(250, r::Optional , d::RepeatCount.bounded(24))),
-              d::LoopDef.build("2100A", d::RepeatCount.bounded(1),
-                s::NM1.use(300, r::Mandatory, d::RepeatCount.bounded(1)),
-                s::PER.use(400, r::Optional, d::RepeatCount.bounded(1)),
-                s::N3.use(500, r::Optional, d::RepeatCount.bounded(1)),
-                s::N4.use(600, r::Optional, d::RepeatCount.bounded(1)),
-                s::DMG.use(800, r::Optional, d::RepeatCount.bounded(1)))))
+                s::DTP.use(250, r::Optional , d::RepeatCount.bounded(24)),
+                d::LoopDef.build("2100A", d::RepeatCount.bounded(1),
+                  s::NM1.use(300, r::Mandatory, d::RepeatCount.bounded(1)),
+                  s::PER.use(400, r::Optional, d::RepeatCount.bounded(1)),
+                  s::N3.use(500, r::Optional, d::RepeatCount.bounded(1)),
+                  s::N4.use(600, r::Optional, d::RepeatCount.bounded(1)),
+                  s::DMG.use(800, r::Optional, d::RepeatCount.bounded(1))))))
 
         end
       end
